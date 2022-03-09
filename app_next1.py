84dbd890b966d999d7af83c31e29db2226753574
Ahmad Samir <wardag.as@yahoo.com>
2022-03-10 02:54:19 +0300

    fixed db

--- app_next1.py
+++ app_next1.py
@@ -25,7 +25,46 @@ authenticator = stauth.authenticate(
 name, authentication_status = authenticator.login('Login', 'main')
 
 if authentication_status:
+    button = st.button('Detect')
+    button_clear = st.button('Clear')
     FRAME_WINDOW = st.image([])
+    FRAME_WINDOW_DETECT = st.image([])
+    name_detected = ''
+
+    def detecters(image, result, face_empty):
+        FRAME_WINDOW_DETECT = st.image([])
+        img = image
+        name_detected = '1'
+        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+        faceCurrFrame = face_recognition.face_locations(imgS)
+        encodings_curret_frame = face_recognition.face_encodings(imgS, faceCurrFrame)
+
+        x, y, w, h = result
+        roi_color = img[y:y + h, x:x + w]
+        res = cv2.cvtColor(roi_color, cv2.COLOR_BGR2RGB)
+        cv2.imwrite('result.png', res)
+        source_image = cv2.imread(f'result.png')
+        source_image = cv2.cvtColor(source_image, cv2.COLOR_BGR2RGB)
+        source_image = cv2.resize(source_image, (360, 400), None, .25, .25)
+
+        frame = np.hstack((source_image, face_empty))
+
+        for encodeFace, faceLoc in zip(encodings_curret_frame, faceCurrFrame):
+            matches = face_recognition.compare_faces(encoding, encodeFace)
+            face_distance = face_recognition.face_distance(encoding, encodeFace)
+            matchIndex = np.argmin(face_distance)
+            name_detected = 'Not found found!!!'
+            if matches[matchIndex]:
+                name_detected = classNames[matchIndex].upper()
+                face = cv2.imread(f'temp_store/{classNames[matchIndex]}.jpg')
+                face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                face = cv2.resize(face, (360, 400), None, .25, .25)
+                name_detected = 'Name: ' + classNames[matchIndex].upper()
+                frame = np.hstack((source_image, face))
+            FRAME_WINDOW_DETECT.image(frame)
+        return name_detected
 
 
     @st.cache(suppress_st_warning=True, allow_output_mutation=True)
@@ -44,47 +83,26 @@ if authentication_status:
 
         img = cv2.resize(img, (720, 400), None, .25, .25)
 
-        face = cv2.imread(f'empty.png')
-        face = cv2.resize(face, (300, 400), None, .25, .25)
-
-        image = np.hstack((img, face))
-
+        face_empty = cv2.imread(f'empty.png')
+        face_empty = cv2.resize(face_empty, (300, 400), None, .25, .25)
         faces = face_cascade.detectMultiScale(img, 1.1, 4)
 
-        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-        faceCurrFrame = face_recognition.face_locations(imgS)
-        encodings_curret_frame = face_recognition.face_encodings(imgS, faceCurrFrame)
-
-        name = ''
-
-        faces = face_cascade.detectMultiScale(imgS, 1.1, 4)
-        if len(faces) > 1:
-            name = "multiple faces!!!"
         if len(faces) == 1:
-            for encodeFace, faceLoc in zip(encodings_curret_frame, faceCurrFrame):
-                matches = face_recognition.compare_faces(encoding, encodeFace)
-                face_distance = face_recognition.face_distance(encoding, encodeFace)
-                matchIndex = np.argmin(face_distance)
-                name = 'Not found found!!!'
-                if matches[matchIndex]:
-                    name = classNames[matchIndex].upper()
-                    face = cv2.imread(f'temp_store/{classNames[matchIndex]}.jpg')
-                    face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-                    face = cv2.resize(face, (300, 400), None, .25, .25)
-                    image = np.hstack((img, face))
-                    name = 'Name: ' + classNames[matchIndex].upper()
-                    print(name)
+            x, y, w, h = faces[0]
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            if button:
+                name_detected = detecters(img, faces[0], face_empty)
+        if button_clear:
+            name_detected = ''
+            FRAME_WINDOW_DETECT = st.empty()
 
-        cv2.putText(image, name, (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+        button = False
+        cv2.putText(img, name_detected, (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
 
-        FRAME_WINDOW.image(image)
+        FRAME_WINDOW.image(img)
         cv2.waitKey(1)
 elif authentication_status == False:
     st.error('Username/password is incorrect')
 elif authentication_status == None:
     st.warning('Please enter your username and password')
-
-
-FRAME_WINDOW = st.image([])
